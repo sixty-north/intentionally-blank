abf5d07be4e5e6ec6158eca72f536c74d3353ffc
Robert Smallshire <rob@sixty-north.com>
2020-01-10 17:40:01 +0100

    Makes the leading formatter more robust

--- source/intentionally_blank/ext/formatters/leading/formatter.py
+++ source/intentionally_blank/ext/formatters/leading/formatter.py
@@ -1,4 +1,5 @@
 from intentionally_blank.formatter import Formatter
+from intentionally_blank.les_iterables import partition_tail
 from intentionally_blank.text import split_indent, is_blank_line
 
 
@@ -14,12 +15,28 @@ class LeadingFormatter(Formatter):
         Yields:
             An iterable series of strings, each with a newline terminator.
         """
+        head, tail = partition_tail(lines, 1)
+
+        # Process all but the last line
         active_indent = ""
-        for line in lines:
+        for line in head:
             indent, text = split_indent(line)
             if not is_blank_line(indent, text):
                 yield line
                 active_indent = indent
             else:
+                assert line.endswith("\n")
+                yield f"{active_indent}\n"
+                
+        # tail will contain at most one line
+        for line in tail:
+            indent, text = split_indent(line)
+            if not is_blank_line(indent, text):
+                yield line
+            else:
                 end = "\n" if line.endswith("\n") else ""
-                yield f"{active_indent}{end}"
+                if len(indent) != 0:
+                    yield f"{active_indent}{end}"
+                else:
+                    yield end
+        
--- tests/ext/visible/test_visible.py
+++ tests/ext/visible/test_visible.py
@@ -1,5 +1,7 @@
 from io import StringIO
 
+import pytest
+
 from intentionally_blank import api
 
 
@@ -7,3 +9,11 @@ def test_format_visible_with_empty_string():
     with StringIO() as in_file, StringIO() as out_file:
         api.format(in_file, out_file, format_names=["visible"])
         assert len(out_file.getvalue()) == 0
+
+
+@pytest.mark.skip("Godkjenn not ready")
+def test_format_visible(airtravel_ragged_file, godkjenn):
+    with StringIO() as out_file:
+        api.format(airtravel_ragged_file, out_file, format_names=["visible"])
+        actual_text = out_file.getvalue()
+        godkjenn.verify(actual_text.encode("utf-8"))
